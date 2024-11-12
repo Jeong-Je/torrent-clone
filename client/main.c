a061900d37097f03f114d72c1fd123e65316b256
Jeong-Je <jeongjeson656@gmail.com>
2024-11-12 22:55:25 +0900

    main함수 명령행 인자 수정

--- client/main.c
+++ client/main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "make_torrent.h"
 
 int main(int argc, char *argv[]) {
-    if (argc == 2) {
-        char *file_path = argv[1];
-        create_torrent(file_path, "113.198.138.212");
+    if (argc == 3) {
+	char *option = argv[1]; 
+        char *file_path = argv[2];
+	if(strcmp(option, "make") == 0) {
+        	create_torrent(file_path, "113.198.138.212");
+	} else if(strcmp(option, "down") == 0) {
+		printf("파일 다운\n");
+	} else {
+		fprintf(stderr, "인자 오류\n");
+		return 1;
+	}
     } else {
-        fprintf(stderr, "사용법: %s <파일 경로>\n", argv[0]);
-        return 1;
+	printf("torrent client program manual\n");
+        printf(".torrent 생성: %s make <파일 경로>\n", argv[0]);
+	printf(".torrent 다운: %s down <파일 경로>\n", argv[0]);
+        return 0;
     }
     return 0;
 }
