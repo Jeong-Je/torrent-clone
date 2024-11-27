91d42ac91c96b58297fd2564718653c37c3eba55
songhj <songhojin913@gmail.com>
2024-11-27 16:49:49 +0900

    request_tracker 리턴값

--- client/request_tracker.h
+++ client/request_tracker.h
@@ -1,6 +1,6 @@
 #ifndef REQUEST_TRACKER_H
 #define REQUEST_TRACKER_H
 
-void request_tracker(char *announce, char *file_name);
+char* request_tracker(char *announce, char *file_name, int* peer_num);
 
 #endif
